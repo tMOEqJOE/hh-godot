6b47b26d82bb271e2266fbd7034cff2929d70817
tMOE <smeardframe@gmail.com>
2025-07-30 23:58:13 -0700

    new 6A and training counter update

--- game/ui/DamageCounter.gd
+++ game/ui/DamageCounter.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 var hp_bar: Node
 
-var last_combo_damage: int
 var highest_combo_damage: int
 
 func tick():
-	$AttackDamage.text = "Attack  Damage: "+str(hp_bar.get_attack_damage())
-	$ComboDamage.text = "Combo Damage: "+str(hp_bar.get_combo_damage())
+	if (hp_bar.get_attack_damage() != 0):
+		$AttackDamage.text = "Attack  Damage: "+str(hp_bar.get_attack_damage())
+	if (hp_bar.get_combo_damage() != 0):
+		$ComboDamage.text = "Combo Damage: "+str(hp_bar.get_combo_damage())
