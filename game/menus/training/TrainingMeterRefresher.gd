4d0449d1fbb165342927aeda196c06e6e79dc95e
tMOE <smeardframe@gmail.com>
2026-02-19 20:41:36 -0800

    New Training features

--- game/menus/training/TrainingMeterRefresher.gd
+++ game/menus/training/TrainingMeterRefresher.gd
@@ -4,11 +4,18 @@ var fighter_game: FighterGame
 
 var prevComboTime = 0
 
+var assist_build: bool = true
+var meter_reset_option: bool = true
+
 func try_meter_refresh(super_meter:int, assist_meter:int, sync_rate:int):
 	if (prevComboTime > 0 and 
 			fighter_game.ServerPlayer.currentState.get(Enums.StKey.comboTime, 0) <= 0 and
 			fighter_game.ClientPlayer.currentState.get(Enums.StKey.comboTime, 0) <= 0):
-		refresh_meter(super_meter, assist_meter, sync_rate)
+		if (meter_reset_option):
+			refresh_meter(super_meter, assist_meter, sync_rate)
+	elif (prevComboTime > 0):
+		fighter_game.ServerPlayer.assist_meter_build_frozen = false
+		fighter_game.ClientPlayer.assist_meter_build_frozen = false
 	else:
 		prevComboTime = fighter_game.ServerPlayer.currentState.get(Enums.StKey.comboTime, 0) + fighter_game.ClientPlayer.currentState.get(Enums.StKey.comboTime, 0)
 
@@ -29,4 +36,6 @@ func refresh_meter(super_meter:int, assist_meter:int, sync_rate:int):
 	fighter_game.get_node("Camera3D/BattleUI/ClientHPBar").reset_hp()
 	fighter_game.get_node("Camera3D/BattleUI/ServerHPBar").reset_hp()
 	fighter_game.get_node("Camera3D/BattleUI/RoundTimer").reset_time()
+	fighter_game.ServerPlayer.assist_meter_build_frozen = not assist_build
+	fighter_game.ClientPlayer.assist_meter_build_frozen = not assist_build
 	prevComboTime = 0
