bfb277eb2c4ce7a581c9ef4c07c6157984bb4bdc
tMOE <smeardframe@gmail.com>
2026-02-22 16:37:12 -0800

    new menu arts

--- game/Global.gd
+++ game/Global.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const BATTLE_ENGINE_VERSION = "HHv0.8.0002"
+const BATTLE_ENGINE_VERSION = "HHv0.8.0003"
 
 func get_battle_version() -> String:
 	if (len(Build.INTERNAL_BATTLE_ENGINE_KEY) < 50):
