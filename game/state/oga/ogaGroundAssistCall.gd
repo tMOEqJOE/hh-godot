b5207dbf642f862fd160b521fed2660d4f44a83b
tMOE <smeardframe@gmail.com>
2026-05-31 15:42:47 -0700

    new combo trials, snapback input change

--- game/state/oga/ogaGroundAssistCall.gd
+++ game/state/oga/ogaGroundAssistCall.gd
@@ -32,7 +32,7 @@ func _init():
 
 func special_cancel(state: Dictionary, interpreter: InputInterpreter):
 	if (state[Enums.StKey.hitStopFrame] >= 0):
-		if (level_1_OK(state) and interpreter.special_input_button(Enums.SpecialInput.M63214, Enums.InputFlags.CDown, state[Enums.StKey.leftface])):
+		if (level_1_OK(state) and interpreter.special_input_button(Enums.SpecialInput.M214, Enums.InputFlags.CDown, state[Enums.StKey.leftface])):
 			change_state.call("Snapback")
 		elif (level_1_OK(state) and interpreter.special_input_button(Enums.SpecialInput.M623, Enums.InputFlags.CDown, state[Enums.StKey.leftface])):
 			change_state.call("HeavyDP")
