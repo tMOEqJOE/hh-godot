7e2631c0d08d355ff936f557bb141ab05f19dcb0
tMOE <smeardframe@gmail.com>
2026-09-01 12:17:15 -0700

    remove stale cache and reset a.oga damage

--- game/state/assist/assistoga/AssistOgaAssistAirSuperFollowupState.gd
+++ game/state/assist/assistoga/AssistOgaAssistAirSuperFollowupState.gd
@@ -19,7 +19,7 @@ func _init():
 			Enums.StKey.Hit1PosX : 24707072, Enums.StKey.Hit1PosY : -11993088,
 			Enums.StKey.Hit1ScaleX : 2737817, Enums.StKey.Hit1ScaleY : -1429710,
 			Enums.StKey.hitstun : 60,
-			Enums.StKey.attack_damage: 100,
+			Enums.StKey.attack_damage: 75,
 			Enums.StKey.meter_build: 0,
 			Enums.StKey.attack_type : Enums.AttackType.WallBouncer,
 			Enums.StKey.guard: Enums.GuardType.Mid,
